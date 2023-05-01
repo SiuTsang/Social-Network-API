cff69f857694186ef6877b4a40ed0ebc748dccdb
siutsang <95592154+royshtsang@users.noreply.github.com>
2023-05-01 04:12:00 -0400

    updated setup

---
+++ controllers/thoughtController.js
@@ -0,0 +1,71 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+  // Get all thoughts
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Get a thought
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__v');
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Create a though
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Delete a thought
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      await User.deleteMany({ _id: { $in: thought.users } });
+      res.json({ message: 'Thought and users deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Update a course
+  async updateCourse(req, res) {
+    try {
+      const course = await Course.findOneAndUpdate(
+        { _id: req.params.courseId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!course) {
+        res.status(404).json({ message: 'No course with this id!' });
+      }
+
+      res.json(course);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
